307fbe4b1f9796c83aeacd552e663dca5a0b4298
Saqrag <saqrag@outlook.com>
2018-04-08 19:57:24 +0800

    support gradle 4.0

--- app/src/main/java/com/zuga/gallery/App.java
+++ app/src/main/java/com/zuga/gallery/App.java
@@ -3,7 +3,6 @@ package com.zuga.gallery;
 import android.app.Application;
 import android.support.v4.content.ContextCompat;
 
-import com.zuga.imagepicker.FrescoImageLoader;
 import com.zuga.imagepicker.ImagePicker;
 import com.zuga.imagepicker.PickerConfig;
 import com.zuga.keyboard.helpers.KeyboardManager;
@@ -24,7 +23,6 @@ public class App extends Application {
         KeyboardManager.init(this);
         SmallVideoDefaultParams.init(this);
         ImagePicker.init(new PickerConfig.Builder().setAppContext(getApplicationContext())
-                .setImageLoader(new FrescoImageLoader(getApplicationContext()))
                 .setToolbaseColor(ContextCompat.getColor(getApplicationContext(), R.color.title_bar_background)).build());
     }
 }
